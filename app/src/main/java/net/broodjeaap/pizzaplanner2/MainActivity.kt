fb6e6cc2cf025b0151e8f9293d65e00ad7e08148
BroodjeAap <davidzessen@gmail.com>
2025-09-13 15:35:43 +0200

    fixed top bar

--- app/src/main/java/net/broodjeaap/pizzaplanner2/MainActivity.kt
+++ app/src/main/java/net/broodjeaap/pizzaplanner2/MainActivity.kt
@@ -10,8 +10,6 @@ import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
 import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import net.broodjeaap.pizzaplanner2.R
@@ -71,16 +69,8 @@ class MainActivity : AppCompatActivity() {
 
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
-        val appBarConfiguration = AppBarConfiguration(
-            setOf(
-                R.id.navigation_recipes,
-                R.id.navigation_planning,
-                R.id.navigation_active,
-                R.id.navigation_settings
-            )
-        )
 
-        setupActionBarWithNavController(navController, appBarConfiguration)
+        // Removed ActionBar setup since we're using NoActionBar theme
         navView.setupWithNavController(navController)
     }
 
